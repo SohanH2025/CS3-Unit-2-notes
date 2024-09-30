8fe1166d1b8a36de16769d3241cbae1dbee62db4
SohanH2025 <sohan.hormuzdiar2025@gbwl.org>
2024-09-30 23:01:21 +0000

    edited main

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
+import numpy as np
 import pandas as pd
 
 
-def main():
-    print("hello world")
+#panda series object
+#series are 10 arrays of index data
+data = pd.Series([1, 2,3,4,5, 6, 7, 8, '9.0',10])
+print(data.values) #looks like list
+print(data.index) #range computed for inicies 
 
+print(f"\ndata at value 1 is {data[1]}\n\nmultiple values shows indicies")
+print(data[1:5])
 
-if __name__ == "__main__":
-    main()
+#series are like arrays but with specific indexs d
+data = pd.Series([1,2,3],index=['a', 'b', 'c'])
+print(data)
