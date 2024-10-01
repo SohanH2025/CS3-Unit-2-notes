d2d93eacbba61209c12acc5467e4fa5dcb79a2ac
SohanH2025 <sohan.hormuzdiar2025@gbwl.org>
2024-10-01 19:11:36 +0000

    poorly did demos

--- main.py
+++ main.py
@@ -14,3 +14,22 @@ print(data[1:5])
 #series are like arrays but with specific indexs d
 data = pd.Series([1,2,3],index=['a', 'b', 'c'])
 print(data)
+
+
+josh = pd.DataFrame(data, columns=['rat'])
+print(josh)
+josh['apples'] = ['apple','bannana', 'ringps']
+print(josh)
+
+
+#data elcioimtn 
+data = pd.Series(['a','c','e'], index=[1,3,5])
+
+#slicing uses inmcplicist index 
+print(data[1:3])
+
+#instead use loc atribute that uses exclicit indics 
+print(data.loc[3:5]) #includes idem at last index 2, 5 too 
+
+#not expliciti ILOC implixcitit locator 
+print(data.iloc[0:1]) #does not include second item 
