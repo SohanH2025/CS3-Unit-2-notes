c4401de68b4d11f8a2bd6e50bcf5629077870733
SohanH2025 <sohan.hormuzdiar2025@gbwl.org>
2024-10-22 19:10:50 +0000

    committed

--- lineplot.py
+++ lineplot.py
@@ -6,7 +6,7 @@ plt.style.use('seaborn-v0_8-dark')
 print(plt.style.available)
 
 #x is indepentent variable 
-x_vals = np.linspace(0,10,100)
+x_vals = np.linspace(0,10,1000)
 
 #line plots are good when graphing functions
 plt.plot(x_vals, np.sin(x_vals))
@@ -21,5 +21,27 @@ fig = plt.figure()
 ax = plt.axes()
 
 ax.plot(x_vals, np.tan(x_vals), color='red')
+ax.plot(x_vals, x_vals, color='green', linestyle='dashed')
+ax.plot(x_vals, 2*x_vals, color='yellow', linestyle='dashed')
+
+ax.plot(x_vals, -x_vals*x_vals, '--c')
 
 fig.savefig('lineplot2.png')
+
+fig3 = plt.figure()
+ax3 = plt.axes()
+
+plt.plot(x_vals, np.sin(x_vals), label ="sin(x)")
+plt.xlim(-1,11)
+plt.ylim(-10,10)
+
+#plt.axis('tight')
+
+#add titles /labels
+plt.title('a sin wave')
+plt.xlabel("x")
+plt.ylabel("sin(x)")
+#add legend 
+plt.legend()
+
+fig3.savefig('lineplot3.png')
